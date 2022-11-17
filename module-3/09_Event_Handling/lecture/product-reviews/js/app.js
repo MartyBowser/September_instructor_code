48a7eb388ac7255aada146061a6b6ae42b6d5797
Christopher Guarnera <christopher@techelevator.com>
2022-11-17 08:39:56 -0500

    updates

--- module-3/09_Event_Handling/lecture/product-reviews/js/app.js
+++ module-3/09_Event_Handling/lecture/product-reviews/js/app.js
@@ -1,11 +1,32 @@
-const book_name = 'Cigar Parties for Dummies';
-let description = 'Host and plan the perfect cigar party for all of your squirrelly friends.';
-let reviews = [
+const book_name = 'Ghost Hunting for Dummies';
+const description = 'Learn how to hunt ghosts like the professionals.';
+const reviews = [
   {
     reviewer: 'Malcolm Madwell',
     title: 'What a book!',
     review:
     "It certainly is a book. I mean, I can see that. Pages kept together with glue and there's writing on it, in some language. Yes indeed, it is a book!",
+    rating: 3
+  },
+  {
+    reviewer: 'Tim Ferriss',
+    title: 'Caught a ghost in less than 4 hours.',
+    review:
+      "It should have been called the four hour ghost hunt. That's amazing.",
+    rating: 4
+  },
+  {
+    reviewer: 'Ramit Sethi',
+    title: 'What every new entrepreneurs needs. A door stop.',
+    review:
+      "When I sell my courses, I'm always telling people that if a book costs less than $20, they should just buy it. If they only learn one thing from it, it was worth it. Wish I learned something from this book.",
+    rating: 1
+  },
+  {
+    reviewer: 'Gary Vaynerchuk',
+    title: 'And I thought I could write',
+    review:
+      "There are a lot of good, solid tips in this book. I don't want to ruin it, but at the end you realize that you yourself are actually a ghost.",
     rating: 3
   }
 ];
